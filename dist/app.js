a63d3b76410245089bbbb0c48d9beacd740783e0
ThiagoRHonda <trossettohonda@gmail.com>
2023-09-25 11:05:04 -0300

    Atualizando para usar http e https

--- dist/app.js
+++ dist/app.js
@@ -6,18 +6,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const https_1 = __importDefault(require("https"));
 const http_1 = __importDefault(require("http"));
+const fs_1 = __importDefault(require("fs"));
 const middlewares_1 = require("./middlewares");
 const routes_1 = require("./routes");
 const app = (0, express_1.default)();
 const port = 3000;
+var privatekey = fs_1.default.readFileSync('selfsigned.key', 'utf8');
+var certificate = fs_1.default.readFileSync('selfsigned.crt', 'utf8');
+var credentials = { key: privatekey, cert: certificate };
 app.use(express_1.default.json());
 app.use(middlewares_1.addTimestamp);
 app.use(middlewares_1.logger);
 app.use('/health', routes_1.healthRouter);
 app.use('/calculator', routes_1.calculatorRouter);
 app.use(middlewares_1.errorHandler);
-http_1.default.createServer(app).listen(port);
-https_1.default.createServer(app).listen(4003);
+http_1.default.createServer(app).listen(port, function () {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+https_1.default.createServer(credentials, app).listen(8443, function () {
+    console.log(`Example app listening at http://localhost:8443`);
+});
 /*app.listen(port, function() {
     console.log(`Example app listening at http://localhost:${port}`);
+});
+app.listen(8443, function() {
+    console.log("Example app listening at http://localhost:8443");
 });*/ 
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import https from 'https';
 import http from 'http';
+import fs from 'fs';
 import { addTimestamp, errorHandler, logger } from './middlewares';
 import { calculatorRouter, healthRouter } from './routes';
+
 const app = express();
 const port = 3000;
+
+var privatekey = fs.readFileSync('selfsigned.key', 'utf8');
+var certificate = fs.readFileSync('selfsigned.crt', 'utf8');
+
+var credentials = {key: privatekey, cert: certificate};
 
 app.use(express.json());
 app.use(addTimestamp);
@@ -14,9 +21,18 @@ app.use('/health', healthRouter);
 app.use('/calculator', calculatorRouter);
 
 app.use(errorHandler);
-http.createServer(app).listen(port)
-https.createServer(app).listen(4003)
+
+http.createServer(app).listen(port, function() {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
+
+https.createServer(credentials, app).listen(8443, function() {
+    console.log(`Example app listening at http://localhost:8443`);
+});
 
 /*app.listen(port, function() {
     console.log(`Example app listening at http://localhost:${port}`);
+});
+app.listen(8443, function() {
+    console.log("Example app listening at http://localhost:8443");
 });*/
